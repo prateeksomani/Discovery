1ab3153eb900c53e7176037980731af227beacbe
prateek <prateek@prateek-PC.mshome.net>
2019-07-19 00:04:43 +0530

    co

---
+++ Discovery/src/test/java/Discovery/Discovery/NewTest1.java
@@ -0,0 +1,20 @@
+package Discovery.Discovery;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+
+public class NewTest1 {
+  @Test
+  public void f() {
+	  System.out.println("prateek");
+  }
+  @BeforeMethod
+  public void beforeMethod() {
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+  }
+
+}
